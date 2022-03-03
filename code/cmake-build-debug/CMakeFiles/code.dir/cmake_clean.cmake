d53f96d6bd424b9564002bf2c3349af91f92c949
XinyiZhou <18317011442@163.com>
2022-03-03 18:19:01 +0800

    initial

---
+++ code/cmake-build-debug/CMakeFiles/code.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/code.dir/main.cpp.obj"
+  "code.exe"
+  "code.exe.manifest"
+  "code.lib"
+  "code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
