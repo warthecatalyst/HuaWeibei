4c1a88f349a147c7a422a98e3df2c20439fcef64
XinyiZhou <18317011442@163.com>
2022-03-14 14:20:36 +0800

    初赛输入部分完成

---
+++ Preliminary/cmake-build-debug/CMakeFiles/Preliminary.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Preliminary.dir/main.cpp.obj"
+  "Preliminary.exe"
+  "Preliminary.exe.manifest"
+  "Preliminary.pdb"
+  "libPreliminary.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Preliminary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Preliminary/main.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <functional>
+#include <memory>
+
+using namespace std;
+
+//客户节点存储的数据
+unordered_map<string,int> clientName_to_ID;
+vector<string> clientID_to_Name;
+vector<vector<int>> demand; //demand[i][j]表示第i个时间节点，第j个客户节点的流量需求
+
+//边缘节点存储的数据
+unordered_map<string,int> serverName_to_ID;
+vector<pair<string,int>> serverID_to_Val;
+
+//客户节点到边缘节点的时延
+vector<vector<int>> client_server_delay;
+
+int QOS;    //最大可以承受的时延
+
+void ProcessClients(istringstream& is){     //主要是处理clientName_to_ID和clientID_to_Name
+    string stn;
+    getline(is,stn,',');
+    int i = 0;
+    while(getline(is,stn,',')){
+        clientName_to_ID[stn] = i;
+        clientID_to_Name.push_back(stn);
+        i++;
+    }
+}
+
+vector<int> ProcessNames(istringstream& is){
+    vector<int> ans;
+    string stn;
+    getline(is,stn,',');
+    while(getline(is,stn,',')){
+        ans.push_back(clientName_to_ID[stn]);
+    }
+    return ans;
+}
+
+void ProcessInput(){
+    ifstream infile("data\\demand.csv",ios::in);
+    if(!infile){
+        cout<<"Can't open file"<<endl;
+        exit(1);
+    }
+    string line;
+    getline(infile,line);
+    istringstream is(line);
+    ProcessClients(is);
+
+//    for(auto &it:clientName_to_ID){
+//        cout<<it.first<<" "<<it.second<<endl;
+//    }
+    //处理带宽值
+    while(getline(infile,line)){
+        vector<int> client_demand;
+        is.clear();
+        is.str(line);
+        string stn;
+        getline(is,stn,',');
+        while(getline(is,stn,',')){
+            client_demand.push_back(stoi(stn));
+        }
+        demand.push_back(client_demand);
+    }
+
+//    for(auto& vec:demand){
+//        for(auto &i:vec){
+//            cout<<i<<" ";
+//        }
+//        cout<<endl;
+//    }
+
+    infile.close();
+    infile.open("data\\site_bandwidth.csv");
+    if(!infile){
+        cout<<"Can't open file"<<endl;
+        exit(1);
+    }
+    getline(infile,line);   //第一列没用
+    int i = 0;
+    while(getline(infile,line)){
+        is.clear();
+        is.str(line);
+        string name,bandwidth;
+        getline(is,name,',');
+        getline(is,bandwidth,',');
+        serverName_to_ID[name] = i++;
+        serverID_to_Val.emplace_back(name,stoi(bandwidth));
+    }
+
+//    for(auto& vec:serverID_to_Val){
+//        cout<<vec.first<<" "<<vec.second<<endl;
+//    }
+
+    infile.close();
+    infile.open("data\\qos.csv");
+    if(!infile){
+        cout<<"Can't open file"<<endl;
+        exit(1);
+    }
+    getline(infile,line);
+    client_server_delay = vector<vector<int>>(clientID_to_Name.size(),vector<int>(serverID_to_Val.size()));
+    is.clear();
+    is.str(line);
+    vector<int> clientIDs = ProcessNames(is);
+    i = 0;
+    while(getline(infile,line)){
+        is.clear();
+        is.str(line);
+        string name,qos;
+        getline(is,name,',');
+        int j = 0,serverId = serverName_to_ID[name];
+        while(getline(is,qos,',')){
+            client_server_delay[j][serverId] = stoi(qos);
+            j++;
+        }
+    }
+//    for(auto& vec:client_server_delay){
+//        for(auto& val:vec){
+//            cout<<val<<" ";
+//        }
+//        cout<<endl;
+//    }
+    infile.close();
+    infile.open("data\\config.ini");
+    if(!infile){
+        cout<<"Can't open file"<<endl;
+        exit(1);
+    }
+    getline(infile,line);
+    getline(infile,line);
+    is.clear();
+    is.str(line);
+    string qo;
+    getline(is,qo,'=');
+    getline(is,qo,'=');
+    QOS = stoi(qo);
+}
+
+int main() {
+    ProcessInput();
+    return 0;
+}
